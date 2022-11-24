310429f6f3e25442e6c43d2eb6945bdb746939ff
muratdem1r <dzcdemir@gmail.com>
2022-11-24 10:54:15 +0300

    temizlik

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import "ol/ol.css";
 import "assets/styles/App.css";
 import MapWrapper from "map/components/MapWrapper";
 
-import store from "store";
-import { Provider } from "react-redux";
-
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <MapWrapper />
-      </div>
-    </Provider>
+    <div className="App">
+      <MapWrapper />
+    </div>
   );
 }
 
